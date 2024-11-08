18587d2607cbafae7d73ce77f2ae80fce1dbd7e6
Clement <clem@gitea.dev>
2024-11-08 04:14:25 +0300

    first commit

---
+++ config.exemple.ts
@@ -0,0 +1,5 @@
+export const config = {
+    WEBSITE_URL: "http://localhost",
+    PORT: "3000",
+    ENVIRONNEMENT: "prod", // dev, prod
+};
--- index.ts
+++ index.ts
@@ -1,1 +1,42 @@
-console.log("Hello via Bun!");
+import {config} from "./config.ts";
+
+Bun.serve({
+    fetch(req: Request) {
+        const url = new URL(req.url);  // Récupère l'URL de la requête
+
+        // Route pour la page d'accueil
+        if (url.pathname === "/" || url.pathname === "/home") {
+            return new Response("Welcome to the Home page!", {
+                headers: { 'Content-Type': 'text/plain' },
+            });
+        }
+
+        // Route pour /torrent
+        if (url.pathname.startsWith("/torrent/")) {
+            const id = url.pathname.split("/")[2];
+            return new Response(
+                `This is the Torrent page with ID: ${id}`, {
+                headers: { 'Content-Type': 'text/plain' },
+            });
+        }
+
+        // Route pour /about
+
+        if (url.pathname === "/about") {
+            return new Response("This is the About page.", {
+                headers: { 'Content-Type': 'text/plain' },
+            });
+        }
+
+        // Route par défaut (404 Not Found)
+        return new Response("Page not found", {
+            status: 404,
+            headers: { 'Content-Type': 'text/plain' },
+        });
+    },
+    port: config.PORT, // Définit le port du serveur
+});
+
+console.log(
+    `Server running on ${config.WEBSITE_URL}${config.ENVIRONNEMENT === 'dev' ? ':' + config.PORT : ''}`
+);
